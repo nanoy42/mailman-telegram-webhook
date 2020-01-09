01fd6f434a1a15124c2781064ca70b3ec6997305
Yoann Pietri <me@nanoy.fr>
2020-01-09 15:29:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Mailman Telegram Webhook
+
+Want to send message to telegram chats when receiving an email on a mailing list ? This script do it for you.
+
+## Installation
+
+### Installation procedure
+
+Create the folder `/usr/lib/python3/dist-packages/mailman_telegram/webhook` and copy the `__init__.py` file inside.
+
+Then copy the `mailam-telegram-webhook.cfg` file to `/ect/mailman3` and edit it :
+
+ * You need to set the token to a valid telegram bot token
+ * If you want, you can specify a global chat id. Messages will be sent to this chat when the archiver is enabled for a mailing-list but no specific chat id is defined. If you leave this chat id empty, nothing happens when the archiver is enabled for a list without specific chat id.
+ * You can specify specific chat id in `[list.list_name]` sections
+
+Then copy the following code to mailman configuration (`/etc/mailman3/mailman.cfg`) :
+```
+[archiver.telegram_webhook]
+class: mailman_telegram_webhook.Archiver
+enable: yes
+configuration: /etc/mailman3/mailman-telegram-webhook.cfg
+```
+
+Note : By default, the archiver will be enable on every list and if a global chat id is defined, messages will be sent from every list to this chat.
+
+### Configuration examples
+
+```
+[global]
+token = 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
+chat_id = 103
+
+[list.contact]
+chat_id = 104
+```
+
+We suppose that contact@my.domain and webmaster@my.domain have the archiver enabled and the token `123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11` (which is an example token) is associated to myBot.
+
+ * A mail sent to contact@my.domain will make myBot send a message to the chat with id 104.
+ * A mail sent to webmaster@my.domain will make myBot send a message to the the chat with id 103.
+
+```
+[global]
+token = 123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11
+
+[list.contact]
+chat_id = 104
+```
+
+We suppose that contact@my.domain and webmaster@my.domain have the archiver enabled and the token `123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11` (which is an example token) is associated to myBot.
+
+ * A mail sent to contact@my.domain will make myBot send a message to the chat with id 104.
+ * A mail sent to webmaster@my.domain will make do nothing.
+
+## Under the hood
+
+This little script acts as an archiver (like Hyperkitty). When a message is received by mailman, if the archiver is configured and enabled on the mailing list, the message is passed to the archiver. The `archive_function` function just sends a message to telegram, according to the configuration.
+
+The code is widely adapted from https://github.com/ondrejkolin/mailman_to_rocketchat and from Hyperkitty.
+
+## FAQ
+
+### How do I obtain a telegram bot token ?
+
+You need to create a bot by speaking with @BotFather (see here for more information : https://core.telegram.org/bots)
+
+### How do I find chat ids ?
+
+You can chat with some specific bots to find the chat id or you can use the bot you created. Invite him on the group or chat with him and take a look to `https://api.telegram.org/bot<token>/getUpdates`
+
+### What looks like the message on telegram ?
+
+The message looks like `New message from {from} to {to} : {subject}`.
+
+Example :
+
+`New message from Yoann Pietri <me@nanoy.fr> to webmaster@my.domain : [Webmaster] Unable to access website`.
+
+### Can I change the message on telegram ?
+
+No cannot change it in the configuration but you can edit the message in the `__init__.py` file, in the `archive_message` function. 
---
+++ __init__.py
@@ -0,0 +1,126 @@
+"""
+Archiver sending message to telegram chats.
+"""
+
+import logging
+
+from mailman.interfaces.archiver import IArchiver
+from zope.interface import implementer
+from mailman.config import config
+from mailman.config.config import external_configuration
+from configparser import NoOptionError
+
+from telegram.ext import Updater
+
+logger = logging.getLogger("mailman.archiver")
+
+
+def _log_error(exc):
+    logger.error("Telegram webhook: %s", exc)
+
+
+@implementer(IArchiver)
+class Archiver(object):
+
+    name = "telegran webhook"
+    keys = [
+        "archived-at",
+        "delivered-to",
+        "from",
+        "cc",
+        "to",
+        "in-reply-to",
+        "message-id",
+        "subject",
+        "x-message-id-hash",
+        "references",
+        "x-mailman-rule-hits",
+        "x-mailman-rule-misses",
+    ]
+    config = None
+
+    def _load_config(self):
+        """Load configuration."""
+        # Read our specific configuration file
+        archiver_config = external_configuration(
+            config.archiver.telegram_webhook.configuration
+        )
+        try:
+            self.token = archiver_config.get("global", "token")
+        except (KeyError, NoOptionError):
+            self.token = None
+            _log_error("No telegram token found in configuration")
+
+        try:
+            self.chat_id = archiver_config.get("global", "chat_id")
+        except (KeyError, NoOptionError):
+            self.chat_id = None
+
+        try:
+            for section in archiver_config.sections():
+                if section.startswith("list."):
+                    list_name = section[5:]
+                    self.chats_id[list_name] = archiver_config[section]["chat_id"]
+                else:
+                    continue
+        except (KeyError, NoOptionError) as e:
+            _log_error(
+                "While parsing the config for lists configuration, there was an error "
+                + str(e.message)
+            )
+
+    def __init__(self):
+        """Load config and prepare telegram bot"""
+        self.chats_id = {}
+        self._load_config()
+        try:
+            self.updater = Updater(token=self.token, use_context=True)
+        except:
+            _log_error("Unable to grab bot")
+
+    def archive_message(self, mlist, msg):
+        """Compute message and send it to telegram."""
+        subject = "No subject"
+        msg_content = msg.get_payload(None)
+        try:
+            subject = msg["subject"]
+        except:
+            pass
+
+        format_dict = {"from": msg["from"], "to": msg["to"], "subject": subject}
+
+        message = "New message from {from} to {to} : {subject}"
+
+        information_msg = message.format(**format_dict)
+        self._send_to_telegram(information_msg, mlist.list_name)
+
+    def _send_to_telegram(self, message, list_name):
+        """
+        Effectively send message to telegram
+
+        It tries to get a specific chat_id and if it doesn't exists, it takes the general chat id.
+        If no general chat id is specified, the messahe is then not sent.
+        """
+        try:
+            chat_id = self.chats_id[list_name]
+        except KeyError:
+            chat_id = self.chat_id
+        if chat_id:
+            try:
+                self.updater.bot.send_message(chat_id=chat_id, text=message)
+            except:
+                _log_error("Unable to send message")
+
+    def list_url(self, mlist):
+        """
+        This doesn't make sense for webhook.
+        But we must implement for IArchiver.
+        """
+        return None
+
+    def permalink(self, mlist, msg):
+        """
+        This doesn't make sense for webhook. 
+        But we must implement for IArchiver.
+        """
+        return None
